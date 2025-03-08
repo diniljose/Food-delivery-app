5f47d46f5f42d81311c471e7450e31a11ec2c213
dinilpjose <diniljosework@gmail.com>
2025-03-08 04:34:18 +0300

    file images added

--- src/admin/units/units.controller.ts
+++ src/admin/units/units.controller.ts
@@ -1,21 +1,40 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res } from '@nestjs/common';
 import { UnitsService } from './units.service';
 import { CreateUnitDto } from './dto/create-unit.dto';
 import { UpdateUnitDto } from './dto/update-unit.dto';
 import { Unit } from 'src/schemas/unit.schema';
+import { ResponseService } from 'src/services/response/response.service';
+import { FastifyReply, FastifyRequest } from 'fastify';
+
 
 @Controller()
 export class UnitsController {
-  constructor(private readonly unitsService: UnitsService) {}
+  constructor(private readonly unitsService: UnitsService,    private readonly responseService: ResponseService,) {}
 
   @Post('create')
   async createOrUpdateUnits(@Body() units: CreateUnitDto[]): Promise<any> {
     return this.unitsService.createOrUpdateUnits(units);
   }
 
-  @Get()
-  async getUnits(): Promise<Unit[]> {
-    return this.unitsService.getAllUnits();
+ 
+  
+
+  @Get('getAll')
+  async getAllItems(@Res() res: FastifyReply): Promise<any> {
+    try {
+      const response =  this.unitsService.getAllUnits();
+      return res.send(
+        this.responseService.sendSuccessResponse({ data: response}),
+      );
+    } catch (error) {
+      return res.send(
+        this.responseService.sendErrorResponse(
+          'Failed to fetch items',
+          'فشل في جلب العناصر',
+          error.message,
+        ),
+      );
+    }
   }
 
   @Delete(':id')
